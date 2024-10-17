74f0f53d683fd870e7895e3d216a5178f2c3571a
Bruno Hirae <brunodeluca117@outlook.com>
2024-10-17 13:21:54 -0300

    feat: add theme toggle

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,12 +1,19 @@
 // IMPORTANT: May need to dynamically import the Mail component to avoid SSR issues.
 import { Mail } from "@/app/mail/mail";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default async function MailDashboard() {
   return (
-    <Mail
-      defaultLayout={[20, 32, 48]}
-      defaultCollapsed={false}
-      navCollapsedSize={4}
-    />
+    <>
+      <div className="absolute bottom-4 left-4">
+        <ThemeToggle />
+      </div>
+
+      <Mail
+        defaultLayout={[20, 32, 48]}
+        defaultCollapsed={false}
+        navCollapsedSize={4}
+      />
+    </>
   );
 }
